dc7471d48907666732ff4430b392cb83fdfa0273
kreeti jaya singh <kreetijaya.singh2006@gmail.com>
2026-02-17 08:18:30 +0530

    Alien Invasion upto alien fleet creation

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,36 +1,142 @@
 import sys
-
 import pygame 
+
+from settings import Settings 
+from ship import Ship
+from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
 
     def __init__(self):
         """Initialize the game, and create game resources"""
+        
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((800,600))
+        
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        
         pygame.display.set_caption("alien invasion")
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
 
-        # set the background colour 
-        self.bg_color = (30,30,230)
-    
+        self._create_fleet()
+        
+        # set the background color
+        self.bg_color = (230, 230, 230)
+
+
     def run_game(self):
         """start the main loop for the game"""
         while True:
-            # watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT : 
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()
 
-            # redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
-
-            # MAKE THE MOST RECENTLY DRAWN SCREEN VISIBLE
-            pygame.display.flip()
+            self._update_screen() 
             self.clock.tick(60)
+            
+           
+            # watch for keyboard and mouse events
+    def _check_events(self):
+        """respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT : 
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """respond to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q :
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        """respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """ create a new bullet and add it to the bullet group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _create_fleet(self):
+        """create the fleet of aliens"""
+        # create an alien and keep adding aliens until there's no room left
+        # spacing between aliens is one alien width and one alien height
+        alien = Alien(self)
+        alien_width, alien_height  = alien.rect.size
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 7* alien_height):
+            while current_x < (self.settings.screen_width - 2* alien_width) :
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+            # finished a row, reset x value and increment y value
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
+        """create an alien and place it in the fleet"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+        
+    def _update_bullets(self):
+        """update position of bullets and get rid of the old bullets"""
+        # update bullet positions
+        self.bullets.update()
+
+        #  get rid of bullets that have disappeared 
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
+            
+    def _update_screen(self):
+        """update images on screen and flip to the new screen"""
+         # redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.ship.blitme()
+        self.aliens.draw(self.screen)
+            
+        # MAKE THE MOST RECENTLY DRAWN SCREEN VISIBLE
+        pygame.display.flip()
+            
 
 if __name__ == '__main__' :
     # make a game instance and run the game 
     ai = AlienInvasion()
     ai.run_game()
+
+    
+
+
+
+
+
+
+
+
+
---
+++ settings.py
@@ -0,0 +1,20 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """initialize the game's settings"""
+        # screen settings
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (240, 100, 180)
+
+        # ship settings
+        self.ship_speed = 1.5
+        
+        # bullet settings
+        self.bullet_speed = 2.0 
+        self.bullet_width = 3
+        self.bullet_height = 15 
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+        
